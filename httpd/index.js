2901aac178e82be525e51287ee830fb2286c4a03
linycken013127 <linycken210127@gmail.com>
2017-11-03 14:59:23 +0800

    加入css&png、修改html

--- httpd/index.js
+++ httpd/index.js
@@ -1,49 +1,52 @@
 'use strict';
 
- let http = require('http');
+let http = require('http');
 
- http.createServer((request, response) => {
- let fs = require('fs');
- let postData = ''; // POST 資料
+http.createServer((request, response) => {
+  let fs = require('fs');
+  let postData = ''; // POST 資料
 
- // 利⽤ 'data' event 消耗掉 data chunk;
- // 'end' event 才會被 fired
- request.on('data', (chunk) => {
- postData += chunk;
+  // 利⽤ 'data' event 消耗掉 data chunk;
+  // 'end' event 才會被 fired
+  request.on('data', (chunk) => {
+    postData += chunk;
 
- console.log(
- ' 接收的 POST data ⽚段 k: [' + chunk + '].'
- );
- });
+    console.log(
+      ' 接收的 POST data ⽚段 k: [' + chunk + '].'
+    );
+  });
 
- request.on('end', () => {
- switch (request.url) {
- case '/':
- fs.readFile('../htdocs/index.html', (err, data) => {
- if (err) {
-console.log(' 檔案讀取錯誤');
- }
- else {
- response.writeHead(200, {
- 'Content-Type': 'text/html'
- });
+  request.on('end', () => {
+    switch (request.url) {
+      case '/':
+      fs.readFile('../htdocs/index.html', (err, data) => {
+        if (err) {
+          console.log(' 檔案讀取錯誤');
+        }
+        else {
+          response.writeHead(200, {
+            'Content-Type': 'text/html'
+            //'Content-Type': "text/css"
+            //'Content-Type': "image/png"
 
- response.write(data);
- response.end();
- }
- });
+          });
 
- break;
+          response.write(data);
+          response.end();
+        }
+      });
 
- default:
- console.log(' 未定義的存取: ' + request.url);
+      break;
 
- response.end();
+      default:
+      console.log(' 未定義的存取: ' + request.url);
 
- break;
- }
- });
- }).listen(8088);
+      response.end();
 
- // log message to Console
- console.log(' 伺服器啓動，連線 url: http://127.0.0.1:8088/');
+      break;
+    }
+  });
+}).listen(8088);
+
+// log message to Console
+console.log(' 伺服器啓動，連線 url: http://127.0.0.1:8088/');
